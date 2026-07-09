8a5fd04e655fab2c75125d012d84381627ed40aa
Muhammad Zaheer — Exploring AI Engineering & MLOps <xaheeru23@gmail.com>
2026-07-10 02:41:16 +0500

    chore: remove puppeteer configuration file

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-    // Direct Puppeteer to store Chrome inside the project directory
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
